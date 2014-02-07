b0cfd80edd700165baea2ac6adc904083adceb50
alexandre.michalon <alexandre.michalon@gmail.com>
2014-02-07 18:09:15 +0100

    Second Commit

--- AndroidAppDogeWeather/src/fr/amichalon/androidappdogeweather/Model/DogeWeather.java
+++ AndroidAppDogeWeather/src/fr/amichalon/androidappdogeweather/Model/DogeWeather.java
@@ -3,15 +3,47 @@
  */
 package fr.amichalon.androidappdogeweather.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.amichalon.androidappdogeweather.Models.Enumerations.WeatherIcon;
+import android.R;
+import android.app.Activity;
+import android.content.res.Resources;
+
 /**
  * @author alexandre.michalon
  *
  */
-public class DogeWeather {
+class DogeWeather {
+	
+	private DogeWeather(
+			Activity activity,
+			Weather weather
+			)
+	{
+		this.activityDogeWeather = activity;
+		this.weather = weather;
+		//this.weatherIcon = weatherIcon;
+		
+		buildLexicalField();
+	}
 
 	private Weather weather;
 	
-	private String[] sentences;
+	private WeatherIcon weatherIcon;
 	
+	private String[] lexicalField;
+	
+	private Activity activityDogeWeather;
+	
+	private void buildLexicalField()
+	{
+		Resources res = activityDogeWeather.getResources();
+		
+		String[] lfAll = res.getStringArray(fr.amichalon.androidappdogeweather.R.array.lexical_field_all);
+		
+		String[] lfWeather = res.getStringArray(weatherIcon.getLexicalFieldId());
+	}
 	
 }
